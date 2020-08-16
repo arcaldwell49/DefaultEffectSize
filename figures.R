aa279963e1e8d2296f231daf1b5ad36223372f76
Andrew Vigotsky <avigotsky@gmail.com>
2020-08-16 01:32:37 -0500

    Added a closed-form version of the figure for d_z and Delta

--- figures.R
+++ figures.R
@@ -116,7 +116,7 @@ ggsave("smd_simulation.pdf",
 
 #geom_line version version
 ggplot() +
-  geom_line(data = subset(out.long, statistic == "Estimate"),
+  geom_line(data = subset(out.long, statistic == "Estimate" & type %in% unique(out.long$type)[1:2]),
               aes(x = rho,
                   y = value,
                   color = sigma,
@@ -124,11 +124,11 @@ ggplot() +
   scale_color_viridis_c(name = expression(sigma["pre"]),
                         trans = "log10",
                        option = "C",
-                       breaks = c(0.1,1,10,100),
-                       labels = c(0.1,1,10,100),
+                       #breaks = c(0.1,1,10,100),
+                       #labels = c(0.1,1,10,100),
                        guide = guide_colorbar(order = 1,
                                               barwidth = 0.5)) +
-  geom_line(data = subset(out.long, statistic == "Standard ~ Error"),
+  geom_line(data = subset(out.long, statistic == "Standard ~ Error" & type %in% unique(out.long$type)[1:2]),
              aes(x = rho,
                   color = sigma,
                  group = sigma,
@@ -141,15 +141,86 @@ ggplot() +
        x = expression("Correlation"~(italic(r)))) +
   scale_x_continuous(breaks = c(-1,0,1),
                      limits = c(-1,1)) +
+  scale_y_continuous(trans="log10") +
   #scale_y_continuous(limits = c(0,10)) +
   theme_minimal() +
   theme(panel.background = element_rect(fill = "#f9f9f9",color = "#ffffff")) +
-  theme(aspect.ratio = 1,
+  theme(aspect.ratio = 0.5,
         strip.placement = "outside")
 
 
 ggsave("smd_simulation_point.pdf",
-       width = 8,
-       height = 4.5)
+       width = 6,
+       height = 3)
 
 
+
+
+
+correct <- function(m) 1 - 3/(4 * m - 1)
+
+closed_form <- pbmcapply::pbmclapply(1:nrow(combos), function(i) {
+  n <- 20
+  sigma <- combos[i,"sigma_pre"]
+  rho <- combos[i,"rho"]
+  dz <- 1/sqrt(2*sigma^2*(1-rho))
+  d_z <- data.frame(sigma = sigma,
+                    rho = rho,
+                    type = 'd["z"]',
+                    Estimate = dz,
+                    Variance = (n-1)/(n*(n-3)) * (1 + dz^2*n) - dz^2/correct(n-1)^2)
+  
+  delta <- 1/sigma
+  delta_pre <- data.frame(sigma = sigma,
+                          rho = rho,
+                          type = 'Delta',
+                          Estimate = delta,
+                          Variance = (n-1)/(n*(n-3)) * (2*(1-rho) + delta^2*n) - delta^2/correct(n-1)^2)
+  rbind(d_z,delta_pre)
+}, mc.cores = parallel::detectCores())
+
+closed_form <- do.call(rbind.data.frame, closed_form)
+closed_form.long <- tidyr::gather(closed_form, statistic, value, 4:5)
+
+closed_form.long$statistic <- factor(closed_form.long$statistic,
+                                     levels = unique(closed_form.long$statistic),
+                                     labels = c(expression(Estimate),
+                                                expression(Standard~Error)))
+
+ggplot() +
+  geom_line(data = subset(closed_form.long, statistic == "Estimate"),
+            aes(x = rho,
+                y = value,
+                color = sigma,
+                group = sigma)) +
+  scale_color_viridis_c(name = expression(sigma["pre"]),
+                        trans = "log10",
+                        option = "C",
+                        #breaks = c(0.1,1,10,100),
+                        #labels = c(0.1,1,10,100),
+                        guide = guide_colorbar(order = 1,
+                                               barwidth = 0.5)) +
+  geom_line(data = subset(closed_form.long, statistic == "Standard ~ Error"),
+            aes(x = rho,
+                color = sigma,
+                group = sigma,
+                y = sqrt(value))) +
+  facet_grid(statistic ~ type,
+             labeller = label_parsed,
+             scales = "free_y",
+             switch = "y") +
+  labs(y = "",
+       x = expression("Correlation"~(italic(r)))) +
+  scale_x_continuous(breaks = c(-1,0,1),
+                     limits = c(-1,1)) +
+  scale_y_continuous(trans="log10") +
+  #scale_y_continuous(limits = c(0,10)) +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "#f9f9f9",color = "#ffffff")) +
+  theme(aspect.ratio = 0.5,
+        strip.placement = "outside")
+
+
+ggsave("smd_simulation_point.pdf",
+       width = 6,
+       height = 3)
